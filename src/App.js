7517d644b7db6f803224e5f95fe66f5e295a37ae
Daniel <danielcast244@gmail.com>
2023-04-30 19:46:03 -0500

    USO DE USEEFFECT Y FINALIZACION DEL REPASO REACT

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hola from './components/Hola';
 
 function App() {
 
-  const user = {
-    nombre: "Paola Tamayo Buritica",
-    edad: "19",
-    color: "Amarillo"
+  // const user = {
+  //   nombre: "Paola Tamayo Buritica",
+  //   edad: "19",
+  //   color: "Amarillo"
+  // }
+
+  // const userFn = nombre => {
+  //   console.log("Ricky")
+  // }
+  const [stateCar, setStateCar] = useState(false);
+  const [contar, setContar] = useState(0);
+
+   useEffect(() => {
+    console.log("Total:" + contar)
+   }, [contar]) 
+
+  const encenderApagar = () => {
+    // setStateCar(!stateCar)
+    setStateCar(prevValue => !prevValue)
+    setContar(contar + 1)
   }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <Hola userInfo={user}/>
+        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <h4>Contar: {contar}</h4>
+        <button onClick={encenderApagar}>Encender / Apagar</button>
       </header>
     </div>
   );
--- src/components/Hola.js
+++ src/components/Hola.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Hola(props) {
+  // const {userInfo, userFn} = props;
+  // const {nombre} = userInfo;
   return (
     <div>
-      <p>Hola mi nombre es {props.userInfo.nombre}, tengo {props.userInfo.edad} y mi color favorito es el {props.userInfo.color}</p>
+      {/* <button onClick={() => props.userFn(nombre)}>Saludar</button> */}
     </div>
   )
 }
